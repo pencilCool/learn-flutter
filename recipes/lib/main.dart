6b7d732fecaf31c098da9210d158c67557e03bc1
pencilCool <yhtangcoder@gmail.com>
2023-07-13 09:36:53 +0800

    show ingredients

--- recipes/lib/main.dart
+++ recipes/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'recipe.dart';
+import 'recipe_detail.dart';
 
 void main() {
   runApp(const RecipeApp());
@@ -26,18 +27,7 @@ class RecipeApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
@@ -61,9 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     context,
                       MaterialPageRoute(
                       builder: (context) {
-
-                        // Todo: repalce reutn with detail
-                        return Text("Detail page");
+                        return RecipeDetail(recipe: Recipe.samples[index]);
                       },
                   ),
                   );
--- recipes/lib/recipe_detail.dart
+++ recipes/lib/recipe_detail.dart
@@ -9,5 +9,48 @@ class RecipeDetail extends StatefulWidget {
   }):super(key: key);
 
   @override
-  State<Rec>
+  State<RecipeDetail> createState() {
+   return _RecipeDetailState();
+  }
+}
+
+
+class _RecipeDetailState extends State<RecipeDetail> {
+  //todo: Add _slider here
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.recipe.label),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image(
+                image: AssetImage(widget.recipe.imageUrl),
+              ),
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Text(widget.recipe.label,
+              style: const TextStyle(fontSize: 18),
+            ),
+
+            Expanded(child: ListView.builder(
+                padding: const EdgeInsets.all(7.0),
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: (BuildContext context,int index) {
+                  final ingredient = widget.recipe.ingredients[index];
+                  return Text('${ingredient.quantity} ${ingredient.measure}  ${ingredient.name}');
+                },
+            ),),
+            // TODO : Add slider() here
+          ],
+        ),
+      ),
+    );
+  }
 }
