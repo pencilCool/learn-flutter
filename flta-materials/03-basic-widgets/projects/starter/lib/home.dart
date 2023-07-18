0da7c663ef14502cafd973bc3ee5a73de18173d8
pencilCool <yhtangcoder@gmail.com>
2023-07-18 19:14:29 +0800

    add base card2

--- flta-materials/03-basic-widgets/projects/starter/lib/home.dart
+++ flta-materials/03-basic-widgets/projects/starter/lib/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'card1.dart';
+import 'card2.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -13,7 +14,7 @@ class HomeState extends State<Home> {
   static List<Widget> pages = <Widget>[
     //TODO: replace with Card1
     const Card1() ,
-    Container(color: Colors.green),
+    const Card2() ,
     Container(color: Colors.blue)
   ];
 
