27a8e47909f19fb6d15765c15b8aa606a74c5ae0
pencilCool <yhtangcoder@gmail.com>
2023-07-19 09:32:23 +0800

    card2 done

--- flta-materials/03-basic-widgets/projects/starter/lib/card2.dart
+++ flta-materials/03-basic-widgets/projects/starter/lib/card2.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'author_card.dart';
+import 'fooderlich_theme.dart';
 
 class Card2 extends StatelessWidget {
   const Card2({super.key});
@@ -21,8 +23,37 @@ class Card2 extends StatelessWidget {
         ),
         child: Column(
           children: [
-            //TODO: add author information
+            const AuthorCard(
+              authorName: 'Mike katz',
+              title: 'Smoothe Connoisser',
+              imageProvider: AssetImage('assets/author_katz.jpeg'),
+            ),
             //TODO: add positioned text
+            Expanded(
+              child: Stack(
+                children: [
+                Positioned(
+                    bottom: 16,
+                    right: 16,
+                    child: Text(
+                      'Recipe',
+                      style: FooderlichTheme.lightTextTheme.headline1,
+                    ),
+                ),
+                Positioned(
+                    bottom: 70,
+                    left: 16,
+                    child: RotatedBox(
+                      quarterTurns: 3,
+                      child: Text(
+                        'Smoothies',
+                        style: FooderlichTheme.lightTextTheme.headline1,
+                      ),
+                    ),
+                ),
+              ],
+              ),
+            ),
           ],
         ),
       ),
